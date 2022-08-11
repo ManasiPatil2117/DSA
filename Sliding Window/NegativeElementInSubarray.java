b3374e6f7d4aa1af8f9e377591ac9e5fd687a23e
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-08-11 10:51:15 +0000

    Day 5

--- Sliding Window/NegativeElementInSubarray.java
+++ Sliding Window/NegativeElementInSubarray.java
@@ -1,17 +1,18 @@
+
 // First Negative Number in every window of size k 
 import java.util.ArrayList;
 
 public class NegativeElementInSubarray {
     public static void main(String[] args) {
-        int a[] = new int[] { -5, -1, -7, 8, -15, 30, -16, -28 };
+        int a[] = new int[] { -5, -1, -7, -5, 8, -15, 30, -16, -28 };
         int i = 0, j = 0, n = a.length, k = 3;
         ArrayList<Integer> al = new ArrayList<>();
         while (j < n) {
             if (a[i] < 0) {
-                if (!al.contains(a[i]))
-                    al.add(a[i]);
+                al.add(a[i]);
             }
             if (j - i + 1 < k) {
+                al.remove(0);
                 j++;
             } else if (j - i + 1 == k) {
                 i++;
