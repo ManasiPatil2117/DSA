4e69abdd70b0435f68e7f68d7a3df557d8006f20
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-08-23 05:20:27 +0000

    Day 14

---
+++ OOPs/SuperKeyword.java
@@ -0,0 +1,25 @@
+class Vehicle {
+    public void Print() {
+        System.out.print("Vehicle's Print() ");
+    }
+}
+
+class FourWheelers extends Vehicle {
+    public void Print() {
+        System.out.print("FourWheelers's Print() ");
+    }
+}
+
+class Car extends FourWheelers {
+    public void Print() {
+        super.Print();
+        System.out.print("Car's Print()");
+    }
+}
+
+public class SuperKeyword {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.Print();
+    }
+}
