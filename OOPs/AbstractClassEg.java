d71ca62846acfaeb58b7c049e6b7c82b5f2713e7
ManasiPatil2117 <manasipatilap2117@gmail.com>
2022-08-24 04:53:50 +0000

    Day 15

---
+++ OOPs/AbstractClassEg.java
@@ -0,0 +1,33 @@
+
+ abstract class A {
+    abstract void m1();
+
+    A() {
+        System.out.println("Red");
+    }
+
+    static void m2() {
+        System.out.println("Orange");
+    }
+}
+
+ class B extends A {
+    void m1() {
+        A.m2();
+        System.out.println("Pink");
+    }
+}
+
+ class C extends B {
+    void m3() {
+        System.out.println("Green");
+    }
+}
+
+public class AbstractClassEg {
+    public static void main(String[] args) {
+        C c = new C();
+        c.m3();
+        c.m1();
+    }
+}
