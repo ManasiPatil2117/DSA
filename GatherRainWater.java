c93c8744ebe6d187e448e2b5fbb214c0e37cb6d8
Manasi <manasipatilap2117@gmail.com>
2022-09-08 22:54:29 +0530

    Gather Rain Water prob

---
+++ GatherRainWater.java
@@ -0,0 +1,27 @@
+public class GatherRainWater {
+    public static void main(String[] args) {
+        int a[] = { 90 ,70 ,20 ,80 ,50 };
+        int n = a.length;
+        int secLargest=0, large = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > large) {
+                secLargest = large;
+                large = a[i];
+            }else if(a[i]>secLargest && a[i]!=large)
+            {
+                secLargest = a[i];
+            }
+        }
+        System.out.println(secLargest+" "+large);
+        int ans = secLargest *n,sum=0;
+        for(int i =0;i<n;i++){
+            if(a[i]==large){
+                continue;
+            }
+            sum+=a[i];
+        }
+        sum+=secLargest;
+        ans = ans-sum;
+        System.out.println(ans);
+    }
+}
