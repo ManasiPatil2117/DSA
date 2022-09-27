da2cb5465a388499c5b2213b4e986443532e173d
Manasi <manasipatilap2117@gmail.com>
2022-09-27 07:18:54 +0530

    Binary to decimal conversion

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,13 @@
+
+public class Binary_to_Decimal {
+    public static int decimal(int n, int de, int base){
+        if(n==0)
+            return de;
+        int last = n%10;
+        de+=last*base;
+        return decimal(n/10, de, base*2);
+    }
+    public static void main(String[] args) {
+        System.out.println(decimal(1010101011,0,1));
+    }
+}
